0ac22c4378395136b74515e36b641ab6d4840c98
VirtualVasquez <mxv647@med.miami.edu>
2020-07-28 18:24:41 -0400

    translator finally works, need chai tests

--- public/translator.js
+++ public/translator.js
@@ -22,6 +22,7 @@ const localeSelect = document.getElementById("locale-select");//american-to-brit
 const translatedSentence = document.getElementById("translated-sentence")
 const errorMsg = document.getElementById("error-msg");
 
+//GENERAL FUNCTIONS//
 String.prototype.indexOfEnd = function(string) {
   var io = this.indexOf(string);
   return io == -1 ? -1 : io + string.length
@@ -32,17 +33,22 @@ String.prototype.replaceAt = function(initIndex, finIndex, replacement){
   }
   return this.substring(0, initIndex) + replacement + this.substring(finIndex);
 }
-
 function handleError(){
   translatedSentence.innerHTML ="";
   return errorMsg.innerHTML = "Error: No text to translate."
 }
-function handleNoMatch(){return translatedSentence.innerHTML = "Everything looks good to me!";}
+function handleNoMatch(){
+  return translatedSentence.innerHTML = "Everything looks good to me!";
+}
 function handleClear(){
   return keyFound, valueFound, startIndex, endIndex, finalTranslation, textInput.value, translatedSentence.innerHTML, errorMsg.innerHTML = ''
 }
-function capitalizeFirstLetter(string) {return string.charAt(0).toUpperCase() + string.slice(1);}
+function capitalizeFirstLetter(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+// END GENERAL FUNCTIONS//
 
+//A-TO-B TRANSLATION FUNCTIONS//
 function timeAToB(string){
   let strArr = string.split(" ");
   let timeArr;
@@ -53,7 +59,6 @@ function timeAToB(string){
       timeArr.splice(colInd, 1, ".");
       strArr[i] =  '<span class="highlight">' + timeArr.join("") + '</span>';
       finalTranslation = strArr.join(" ");
-      // return translatedSentence.innerHTML = finalTranslation;
     }    
   }
 }
@@ -66,15 +71,12 @@ function stringAToB(string){
       startIndex = string.indexOf(keyFound);
       endIndex = string.indexOfEnd(keyFound);
       finalTranslation = string.replaceAt(startIndex, endIndex, valueFound);
-      // return translatedSentence.innerHTML = finalTranslation;
     }    
   }
 }
-
 function titleAToB(string){
-  let lowercase = string.toLowerCase(); //keys:values are all lowercase
-  let queryKeys = Object.keys(americanToBritishTitles); // in A->B, we need to match Key
- 
+  let lowercase = string.toLowerCase(); 
+  let queryKeys = Object.keys(americanToBritishTitles); 
   for (let i = 0; i < queryKeys.length; i++){
      if (lowercase.indexOf(queryKeys[i]) !== -1){
        keyFound = queryKeys[i];
@@ -82,11 +84,9 @@ function titleAToB(string){
        startIndex = lowercase.indexOf(keyFound);
        endIndex = lowercase.indexOfEnd(keyFound);
        finalTranslation = string.replaceAt(startIndex, endIndex, valueFound);
-       // return translatedSentence.innerHTML = finalTranslation;
      }    
    }
 }
-
 function translateAToB(string){ 
  timeAToB(string);
  if(finalTranslation !== ""){
@@ -102,58 +102,78 @@ function translateAToB(string){
  }
  handleNoMatch();
 }
+//END A-TO-B TRANSLATION FUNCTIONS//
 
-// function timeBToA(string){
-//   let strArr = string.split(" ");
-//   let timeArr;
-//   for (let i = 0; i < strArr.length; i++){
-//     if (bTimeRegex.test(strArr[i])){
-//       let perInd= strArr[i].indexOf(".");
-//       let timeArr = strArr[i].split("");
-//       timeArr.splice(perInd, 1, ":");
-//       strArr[i] = '<span class="highlight">' + timeArr.join("") + '</span>';
-//       finalTranslation = strArr.join(" ");
-//       return translatedSentence.innerHTML = finalTranslation;
-//     }    
-//   }
-// }
-// function getKeyByValue(object, value){
-//   return Object.keys(object).find(key => object[key] === value);
-// }
-// function translateBToA(string){
-//  keyFound, valueFound, startIndex, endIndex, finalTranslation = '';
-//  if (textInput.value == ''){
-//    return handleError();
-//  }
-// //check if translation target is time, simpler target
-//  timeBToA(string);
-//  //stop here if that was it
-//  if(finalTranslation !== ''){
-//    return 
-//  }
-//  //move to B->A translation otherwise
-//  let queryValues = Object.values(databaseAB); // in B->A, we need to match value
+//B-TO-A TRANSLATION FUNCTIONS//
+function timeBToA(string){
+  let strArr = string.split(" ");
+  let timeArr;
+  for (let i = 0; i < strArr.length; i++){
+    if (bTimeRegex.test(strArr[i])){
+      let perInd= strArr[i].indexOf(".");
+      let timeArr = strArr[i].split("");
+      timeArr.splice(perInd, 1, ":");
+      strArr[i] = '<span class="highlight">' + timeArr.join("") + '</span>';
+      finalTranslation = strArr.join(" ");
+      // return translatedSentence.innerHTML = finalTranslation;
+    }    
+  }
+}
+function getKeyByValue(object, value){
+  return Object.keys(object).find(key => object[key] === value);
+}
+function stringBToA(string){
+  let queryValues = Object.values(databaseAB); // in B->A, we need to match value
 
-//  for (let i = 0; i < queryValues.length; i++){
-//     if (string.indexOf(queryValues[i]) !== -1){
-//       valueFound = queryValues[i];
-//       keyFound = '<span class="highlight">' + getKeyByValue(databaseAB, valueFound) + '</span>';
-//       startIndex = string.indexOf(valueFound);
-//       endIndex = string.indexOfEnd(valueFound);
-//       finalTranslation = string.replaceAt(startIndex, endIndex, keyFound);
-//       return translatedSentence.innerHTML = finalTranslation;
-//     }    
-//   }
-//   //if no match
-//   handleNoMatch(); 
-// }
+ for (let i = 0; i < queryValues.length; i++){
+    if (string.indexOf(queryValues[i]) !== -1){
+      valueFound = queryValues[i];
+      keyFound = '<span class="highlight">' + getKeyByValue(databaseAB, valueFound) + '</span>';
+      startIndex = string.indexOf(valueFound);
+      endIndex = string.indexOfEnd(valueFound);
+      finalTranslation = string.replaceAt(startIndex, endIndex, keyFound);
+      return translatedSentence.innerHTML = finalTranslation;
+    }    
+  }
+}
+function titleBToA(string){
+  let lowercase = string.toLowerCase(); //keys:values are all lowercase
+  let queryValues = Object.values(americanToBritishTitles); // in B->A, we need to match value
+
+  for (let i = 0; i < queryValues.length; i++){
+    if (lowercase.indexOf(queryValues[i]) !== -1){
+      valueFound = queryValues[i];
+      keyFound = '<span class="highlight">' + capitalizeFirstLetter(getKeyByValue(americanToBritishTitles, valueFound)) + '</span>';
+      startIndex = string.indexOf(capitalizeFirstLetter(valueFound));
+      endIndex = string.indexOfEnd(capitalizeFirstLetter(valueFound));
+      finalTranslation = string.replaceAt(startIndex, endIndex, keyFound);
+    }    
+  }
+}
+function translateBToA(string){
+ timeBToA(string);
+ if(finalTranslation !== ''){
+   return translatedSentence.innerHTML = finalTranslation;
+ }
+ stringBToA(string)
+ if(finalTranslation !== ''){
+  return translatedSentence.innerHTML = finalTranslation;
+ }
+ titleBToA(string)
+ if(finalTranslation !== ''){
+  return translatedSentence.innerHTML = finalTranslation;
+ }
+ handleNoMatch(); 
+}
+//END B-TO-A TRANSLATION FUNCTIONS//
+
+
+//ULTIMATE TRANSLATOR FUNCTION//
 function handleTranslate(string){
   if (string == ''){
     return handleError();
   }
-
   keyFound,valueFound,startIndex,endIndex,finalTranslation = '';
-
   if(localeSelect.value == "american-to-british"){
     translateAToB(string);
   }
@@ -161,7 +181,9 @@ function handleTranslate(string){
     translateBToA(string);
   }
 }
+//END ULTIMATE TRANSLATOR FUNCTION//
 
+//EVENT LISTENERS//
 document.addEventListener("click", function(event){
   if(event.target.matches("#clear-btn")){
     handleClear()
@@ -170,6 +192,7 @@ document.addEventListener("click", function(event){
     handleTranslate(textInput.value);
   } 
 })
+//END EVENT LISTENERS//
 
 try {
   module.exports = {
